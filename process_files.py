3bd426aa110b89cdf9478021536a0c5025d82b2d
suchitaj <suchita@gmail.com>
2016-03-12 23:50:20 -0800

    added process_files.py generated directories - endocrine_system, hemic_and_lymphatic, respiratory_tract, skin_and_connective_tissue.

--- process_files.py
+++ process_files.py
@@ -2,6 +2,10 @@
 '''
 ./process_files.py neuropsychiatric-csv-OMIM neuropsychiatric-txt-Entrez neuropsychiatric
 ./process_files.py neurodegenerative-csv-OMIM neurodegenerative-txt-Entrez neurodegenerative
+./process_files.py endrocrine_system-csv-OMIM endrocrine_system-txt-Entrez endrocrine_system
+./process_files.py hemic_and_lymphatic-csv-OMIM hemic_and_lymphatic-txt-Entrez hemic_and_lymphatic
+./process_files.py respiratory_tract-csv-OMIM respiratory_tract-txt-Entrez respiratory_tract
+./process_files.py skin_and_connective_tissue-csv-OMIM skin_and_connective_tissue-txt-Entrez skin_and_connective_tissue
 
 '''
 import csv
@@ -14,12 +18,16 @@ def processCsvDir(dirname, sep, diseaseMap):
         print(filename)
         prefix = filename.split('.')[0]
         diseaseMap[prefix] = set()
-        with open(dirname + '/' + filename, 'rb') as csvfile:
+        with open(dirname + '/' + filename, 'r') as csvfile:
             csvreader = csv.reader(csvfile, dialect='excel', delimiter=sep, quotechar='"')
+            #csvreader = csv.reader(csvfile, delimiter=sep, quotechar='"')
+            i = 0
             for row in csvreader:
+                #print(i, row)
+                i += 1
                 if row[0] and row[0][0] in ['+', '*']:
                     entrezId = row[5]
-                    print(entrezId, ', '.join(row))
+                    #print(entrezId, ', '.join(row))
                     diseaseMap[prefix].add(entrezId)
     return
 
@@ -28,12 +36,12 @@ def processTabDir(dirname, sep, diseaseMap):
     for filename in os.listdir(dirname):
         print(filename)
         prefix = filename.split('.')[0]
-        with open(dirname + '/' + filename, 'rb') as csvfile:
-            csvreader = csv.reader(csvfile, dialect='excel', delimiter=sep, quotechar='"')
+        with open(dirname + '/' + filename, 'r') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=sep, quotechar='"')
             for row in csvreader:
                 entrezId = row[2].strip()
                 if row[2].isdigit():
-                    print(entrezId, ', '.join(row))
+                    #print(entrezId, ', '.join(row))
                     diseaseMap[prefix].add(entrezId)
     return
 
